1a75a824b8b12b4f267c96022f177ec0637b53ec
txngiam <86541241+txngiam@users.noreply.github.com>
2023-06-27 19:19:04 +0200

    Fix position and rotation reset functions

--- Assets/Scripts/ScaleRepositionPlayer.cs
+++ Assets/Scripts/ScaleRepositionPlayer.cs
@@ -1,27 +1,45 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-public class ScalePlayer : MonoBehaviour
+public class ScaleRepositionPlayer : MonoBehaviour
 {
     [SerializeField]
     private float Scale = 1.0f;
 
     public GameObject SpawnPoint;
+    private Transform Player;
+    private Transform Camera;
 
     private void Resize(GameObject Player)
     {
         Player.transform.localScale = Vector3.one * Scale;
     }
 
-    private void ResetPlayerPosition(GameObject Player, GameObject Position)
+    private void ResetPlayerPosition()
     {
-        if (Position == null) return;
-        Player.transform.position = Position.transform.position;
+        if (Player == null) return;
+        // Reset position
+        Vector3 offset = Camera.position - Player.position;
+        offset.y = 0;
+        Player.position = SpawnPoint.transform.position - offset;
+
+        // Reset rotation
+        Vector3 SpawnPointForward = SpawnPoint.transform.forward;
+        SpawnPointForward.y = 0;
+        Vector3 CameraForward = Camera.forward;
+        CameraForward.y = 0;
+
+        float angle = Vector3.SignedAngle(CameraForward, SpawnPointForward, Vector3.up);
+
+        Player.RotateAround(Camera.position, Vector3.up, angle);
     }
 
     void Start()
     {
-        GameObject Player = GameObject.FindGameObjectWithTag("Player");
-        Resize(Player);
-        ResetPlayerPosition(Player, SpawnPoint);
+        GameObject PlayerObj = GameObject.FindGameObjectWithTag("Player");
+        Player = PlayerObj.transform;
+        GameObject CameraObj = GameObject.FindGameObjectsWithTag("MainCamera")[0];
+        Camera = CameraObj.transform;
+        Resize(PlayerObj);
+        ResetPlayerPosition();
     }
 }
